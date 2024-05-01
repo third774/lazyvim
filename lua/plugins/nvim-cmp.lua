83bd85d92552138f386c19c2d579a8e10ad4f58c
Kevin Kipp <kevin.kipp@gmail.com>
2024-04-30 23:28:02 -0500

    Show preview and add borders

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,8 +1,16 @@
 return {
   "hrsh7th/nvim-cmp",
-  opts = {
-    completion = {
-      completeopt = "preview,menu,menuone,noinsert",
-    },
-  }
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require('cmp')
+    return vim.tbl_deep_extend("force", opts, {
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered()
+      },
+      completion = {
+        completeopt = "preview,menu,menuone,noinsert",
+      },
+    })
+  end,
 }
