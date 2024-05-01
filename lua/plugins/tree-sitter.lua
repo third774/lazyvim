940e90c919a636db0785117323a306b45b7812ed
Kevin Kipp <kevin.kipp@gmail.com>
2024-04-30 20:34:15 -0500

    Update ensure_installed and use markdown for mdx

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,8 +1,25 @@
+vim.filetype.add({
+  extension = {
+    mdx = 'markdown'
+  }
+})
+
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
       "astro",
-    },
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml", },
   },
 }
