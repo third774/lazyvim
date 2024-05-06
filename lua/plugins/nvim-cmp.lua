8867ffcc090282740deb4e54de34bf40d3479e7c
Kevin Kipp <kevin.kipp@gmail.com>
2024-05-06 00:19:05 -0500

    Disable auto completion suggestions in comments

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -11,6 +11,18 @@ return {
       completion = {
         completeopt = "preview,menu,menuone,noinsert",
       },
+      -- https://github.com/hrsh7th/nvim-cmp/wiki/Advanced-techniques#disabling-completion-in-certain-contexts-such-as-comments
+      enabled = function()
+        -- disable completion in comments
+        local context = require 'cmp.config.context'
+        -- keep command mode completion enabled when cursor is in a comment
+        if vim.api.nvim_get_mode().mode == 'c' then
+          return true
+        else
+          return not context.in_treesitter_capture("comment")
+              and not context.in_syntax_group("Comment")
+        end
+      end
     })
   end,
 }
