7cba6eadc7408ffc19fa14e46f3178b3b2893def
Kevin Kipp <kkipp@cloudflare.com>
2024-05-11 23:50:50 -0500

    Use tab for autocompletion acceptance

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,27 +2,30 @@ return {
   "hrsh7th/nvim-cmp",
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local cmp = require('cmp')
+    local cmp = require("cmp")
     return vim.tbl_deep_extend("force", opts, {
       window = {
         completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered()
+        documentation = cmp.config.window.bordered(),
       },
       completion = {
         completeopt = "preview,menu,menuone,noinsert",
       },
+      mapping = vim.tbl_deep_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.config.disable,
+        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+      }),
       -- https://github.com/hrsh7th/nvim-cmp/wiki/Advanced-techniques#disabling-completion-in-certain-contexts-such-as-comments
       enabled = function()
         -- disable completion in comments
-        local context = require 'cmp.config.context'
+        local context = require("cmp.config.context")
         -- keep command mode completion enabled when cursor is in a comment
-        if vim.api.nvim_get_mode().mode == 'c' then
+        if vim.api.nvim_get_mode().mode == "c" then
           return true
         else
-          return not context.in_treesitter_capture("comment")
-              and not context.in_syntax_group("Comment")
+          return not context.in_treesitter_capture("comment") and not context.in_syntax_group("Comment")
         end
-      end
+      end,
     })
   end,
 }
