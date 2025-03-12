e3752b02fac47fe8ccf7f2a2c8bfa0ba378c5176
Kevin Kipp <kkipp@cloudflare.com>
2025-03-12 09:11:22 -0500

    Disable auto brackets

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,13 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    completion = {
+      accept = {
+        auto_brackets = {
+          enabled = false,
+        },
+      },
+    },
     keymap = {
       preset = "super-tab",
       ["<C-y>"] = { "select_and_accept" },
