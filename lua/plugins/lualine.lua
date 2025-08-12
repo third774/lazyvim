811fa51b43c8bb0c0c629bf8e66c4b9464a746ec
Kevin Kipp <kkipp@cloudflare.com>
2025-08-12 12:18:54 -0500

    Fix lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,6 +9,20 @@ return {
         icon_only = false,
         colored = true,
       })
+      -- Configure filename to not truncate
+      opts.sections.lualine_c = {
+        {
+          "filename",
+          path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+          shorting_target = 0, -- Don't shorten path
+          symbols = {
+            modified = "[+]",
+            readonly = "[-]",
+            unnamed = "[No Name]",
+            newfile = "[New]",
+          },
+        },
+      }
     end,
   },
 }
