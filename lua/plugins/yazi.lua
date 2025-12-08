5821cd876146e1ad44dfbce2b9be274f6c0746e7
Kevin Kipp <kkipp@cloudflare.com>
2025-12-08 11:57:02 -0600

    Update yazi keymap

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -9,14 +9,14 @@ return {
   keys = {
     -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>z",
+      "<leader>i",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
     {
       -- Open in the current working directory
-      "<leader>Z",
+      "<leader>I",
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
